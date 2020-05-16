c0ea057fc96d19cfdc47c520f23b72744db74dec
theryanclausen <theryanclausen@gmail.com>
2020-05-16 14:15:55 -0700

    no graphql-tools

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,8 @@
-import { importSchema } from 'graphql-import'
-import { makeExecutableSchema } from 'graphql-tools'
-const typeDefs = importSchema('src/schema/schema.graphql');
+// import { importSchema } from 'graphql-import'
+// import { makeExecutableSchema } from 'graphql-tools'
+// const typeDefs = importSchema('src/schema/schema.graphql');
 
-const schema = makeExecutableSchema({ typeDefs,resolvers:  {Query: {
-        hello: () => 'Hello world!',
-      }}});
-export default schema 
+// const schema = makeExecutableSchema({ typeDefs,resolvers:  {Query: {
+//         hello: () => 'Hello world!',
+//       }}});
+// export default schema 
