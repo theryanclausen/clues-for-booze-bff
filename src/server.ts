044bfa504cb9b1f239b013c1f679b5412557a9f2
theryanclausen <theryanclausen@gmail.com>
2020-05-16 13:50:22 -0700

    life check at root

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ const server = new ApolloServer({schema});
 const app = express();
 server.applyMiddleware({ app });
 
+app.get('/', (req,res) => res.send("hello"))
+
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
